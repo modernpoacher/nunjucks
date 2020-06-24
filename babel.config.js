1718a9ee1b7636302325105197b23efcbb663669
Jonathan Perry <jonathanperry@modernpoacher.com>
2020-06-24 17:28:11 +0100

    Babel

--- babel.config.js
+++ babel.config.js
@@ -4,12 +4,9 @@ const log = debug('@modernpoacher/nunjucks')
 
 const {
   env: {
-    DEBUG = '@modernpoacher/nunjucks',
     NODE_ENV = 'development'
   }
 } = process
-
-debug.enable(DEBUG)
 
 log('`modernpoacher/nunjucks` is awake')
 
